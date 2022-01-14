972915cfdb2dd8bef8a258b4b4f086f0469f411f
elkinguerrero007 <3439@holbertonschool.com>
2022-01-14 02:05:06 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ monty.h
@@ -0,0 +1,67 @@
+#ifndef MONTY_H
+#define MONTY_H
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define DELIM "\t\n "
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct glovar - global variable structs
+ * @stackorqueue: value to switch between modes
+ * @fp: File descriptor.
+ * Description: global variables to switch mode
+ */
+typedef struct glovar
+{
+  FILE *fp;
+  int stackorqueue;
+} glovar_t;
+
+glovar_t globales;
+
+void readfile(void);
+bool push_validate(char *push_value, int line_n);
+int get_opcode(const char *opcode, stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+size_t lililen(stack_t **stack);
+void push(stack_t **stack, unsigned int line_number, int push_value);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
