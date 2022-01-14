cba57b82358f69a0c3db0003fa10feadbc3443f6
elkinguerrero007 <3439@holbertonschool.com>
2022-01-14 01:57:40 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ m_pushvalidate.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * push_validate - validation for int value to push
+ * @push_value: int to push
+ * @line_n: line number of the .m file
+ * Return: true if valid or false if not
+ */
+
+bool push_validate(char *push_value, int line_n)
+{
+  int i = 0;
+  bool test = true;
+
+  while (push_value[i])
+    {
+
+      if ((push_value[i] >= 48 && push_value[i] <= 57) || push_value[i] == 45)
+	test = true;
+      else
+	{
+	  test = false;
+	  break;
+	}
+      i++;
+    }
+
+  if (test == false)
+    {
+      fprintf(stderr, "L%d: usage: push integer\12", line_n);
+      exit(EXIT_FAILURE);
+    }
+  return (test);
+}
